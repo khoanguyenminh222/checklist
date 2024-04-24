b82d630635ca23be888397a4998fb9fd975a4e48
Khoa Minh <khoanguyenminh222@gmail.com>
2024-04-24 16:13:28 +0700

    save login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,8 +1,9 @@
 // LoginScreen.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, TextInput, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'
 import Checkbox from 'expo-checkbox';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useUser } from '../UserProvider';
 import axios from 'axios';
 import { domain, userRoute } from '../api/BaseURL';
@@ -16,9 +17,29 @@ const Login = ({ navigation }) => {
   const [isChecked, setChecked] = useState(false);
   const [message, setMessage] = useState('');
   const [toastKey, setToastKey] = useState(0);
+
+  // Khi màn hình đăng nhập được tải, kiểm tra xem có thông tin đăng nhập đã được lưu trong AsyncStorage hay không
+  useEffect(() => {
+    const retrieveLoginInfo = async () => {
+      try {
+        const storedUsername = await AsyncStorage.getItem('username');
+        const storedPassword = await AsyncStorage.getItem('password');
+        if (storedUsername && storedPassword) {
+          setUsername(storedUsername);
+          setPassword(storedPassword);
+          setChecked(true); // Đánh dấu checkbox "Remember"
+          handleLogin(); // Tự động đăng nhập
+        }
+      } catch (error) {
+        console.error('Lỗi khi lấy thông tin đăng nhập từ AsyncStorage:', error);
+      }
+    };
+
+    retrieveLoginInfo();
+  }, []);
+
   const handleLogin = async () => {
     // Thực hiện xác thực đăng nhập, sau đó lưu thông tin người dùng vào Context
-
     try {
       if(!username){
         setMessage('Vui lòng điền username');
@@ -35,6 +56,12 @@ const Login = ({ navigation }) => {
         updateUser(user);
         setMessage('Đăng nhập thành công');
         setToastKey(prevKey => prevKey + 1);
+
+        if (isChecked) {
+          // Nếu người dùng đã chọn "Remember", lưu thông tin đăng nhập vào AsyncStorage
+          await AsyncStorage.setItem('username', username);
+          await AsyncStorage.setItem('password', password);
+        }
 
         if(user.role==='admin'){
           navigation.navigate('MainAdmin'); // Chuyển đến màn hình Home sau khi đăng nhập thành công
@@ -64,14 +91,14 @@ const Login = ({ navigation }) => {
     <SafeAreaView className="flex-1 justify-center items-center bg-white">
       <View className="w-10/12">
         <Text className="text-2xl font-bold mb-5 text-center">Đăng nhập</Text>
-        <TextInput className="h-9 border border-gray-300 rounded mb-3 px-2" placeholder="Tên nhân viên" value={username} onChangeText={setUsername} />
-        <View className="flex flex-row items-center">
-          <TextInput className="flex-1 h-9 border border-gray-300 rounded px-2" placeholder="Mật khẩu" value={password} onChangeText={setPassword} secureTextEntry={secureTextEntry} />
+        <TextInput className="h-14 border border-gray-300 rounded mb-3 px-2" placeholder="Tên nhân viên" value={username} onChangeText={setUsername} />
+        <View className="flex flex-row items-center mb-3">
+          <TextInput className="flex-1 h-14 border border-gray-300 rounded px-2" placeholder="Mật khẩu" value={password} onChangeText={setPassword} secureTextEntry={secureTextEntry} />
           <TouchableOpacity className=" absolute right-2 items-center" onPress={toggleSecureEntry}>
             <Ionicons name={secureTextEntry ? 'eye-off-outline' : 'eye-outline'} size={22} color="black" />
           </TouchableOpacity>
         </View>
-        <View className="flex flex-row items-center">
+        <View className="flex flex-row items-center mb-3">
           <Checkbox
             className="my-2 mr-2"
             value={isChecked}
--- src/screens/Report.js
+++ src/screens/Report.js
@@ -1,12 +1,73 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet  } from 'react-native'
 import React from 'react'
 
 const Report = () => {
+  const reportData = {
+    customerName: "Minh Khoa",
+    district: "Thành phố Vũng Tàu",
+    phoneNumber: "0969606095",
+    address: "55A Ngô Đức Kế",
+    checkedItems: [
+      "6627356d9548261c65f4fd42",
+      "662735999548261c65f4fd46"
+    ],
+    date: "2024-04-24T08:49:32.588Z",
+    note: ""
+  };
   return (
-    <View>
-      <Text>Report</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Báo cáo</Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.label}>Tên khách hàng:</Text>
+        <Text style={styles.value}>{reportData.customerName}</Text>
+      </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.label}>Quận/Huyện:</Text>
+        <Text style={styles.value}>{reportData.district}</Text>
+      </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.label}>Số điện thoại:</Text>
+        <Text style={styles.value}>{reportData.phoneNumber}</Text>
+      </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.label}>Địa chỉ:</Text>
+        <Text style={styles.value}>{reportData.address}</Text>
+      </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.label}>Các mục đã kiểm tra:</Text>
+        {reportData.checkedItems.map((item, index) => (
+          <Text key={index} style={styles.value}>{item}</Text>
+        ))}
+      </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.label}>Ngày tạo:</Text>
+        <Text style={styles.value}>{reportData.date}</Text>
+      </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.label}>Ghi chú:</Text>
+        <Text style={styles.value}>{reportData.note}</Text>
+      </View>
     </View>
   )
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  infoContainer: {
+    marginBottom: 10,
+  },
+  label: {
+    fontWeight: 'bold',
+  },
+  value: {
+    marginLeft: 10,
+  },
+});
 export default Report

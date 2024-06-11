fa1203d2ffd371f2774d76e659bce05ccda23f10
Khoa Minh <khoanguyenminh222@gmail.com>
2024-06-11 15:15:20 +0700

    api

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,27 +4,27 @@ import { Ionicons } from '@expo/vector-icons'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useUser } from '../UserProvider';
 
-const Header = ({screenName, navigation, goback}) => {
-    const { user, logout } = useUser();
-    const handleLogout = async () => {
-      await AsyncStorage.removeItem('username');
-      await AsyncStorage.removeItem('password');
-        logout();
-        navigation.navigate('Login');
-    }
-    const handleBack = () => {
-      navigation.goBack();
-    }
+const Header = ({ screenName, navigation, goback }) => {
+  const { user, logout } = useUser();
+  const handleLogout = async () => {
+    await AsyncStorage.removeItem('username');
+    await AsyncStorage.removeItem('password');
+    logout();
+    navigation.navigate('Login');
+  }
+  const handleBack = () => {
+    navigation.goBack();
+  }
   return (
     <View className="flex flex-row justify-between items-center bg-white pt-11 pb-4 px-2 mb-3 border-b border-gray-500">
-        <View className="flex flex-row items-center">
-          {goback &&<Ionicons name="chevron-back-outline" size={28} onPress={handleBack}></Ionicons>}
-          <Text className="text-2xl font-bold">{screenName}</Text>
-        </View>
-        <View className="flex flex-row items-center">
-            {user && <Text className="text-sm mr-2">Xin chào: {user.fullname}</Text> }
-            <Ionicons name="log-out-outline" size={28} onPress={handleLogout}></Ionicons>
-        </View>
+      <View className="flex-1 flex-row items-center">
+        {goback && <Ionicons name="chevron-back-outline" size={28} onPress={handleBack} />}
+        <Text className="text-2xl font-bold ml-2 flex-shrink" numberOfLines={1} ellipsizeMode="tail">{screenName}</Text>
+      </View>
+      <View className="flex-1 flex-row items-center justify-end">
+        {user && <Text className="text-sm mr-2 truncate" numberOfLines={1} ellipsizeMode="tail">Xin chào: {user.fullname}</Text>}
+        <Ionicons name="log-out-outline" size={28} onPress={handleLogout} />
+      </View>
     </View>
   )
 }
